05276d8f10e89e9e06bfc5fb5023184fca0ecc3f
Ansh Malviya <anshmalviya54@gmail.com>
2024-09-26 00:34:51 +0530

    Added overlay menu + visual bug fixes

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ const playIcon = document.getElementsByClassName("play-icon")[0];
 const pauseIcon = document.getElementsByClassName("pause-icon")[0];
 
 const floatingChatButton = document.getElementsByClassName("floating-chat-button")[0];
-const htmlElement = document.getElementsByTagName("html")[0];
 const lowerFooterElement = document.getElementsByClassName("footer-bottom")[0];
+
+const menuButton = document.getElementById("nav-menu");
+const menuBarIcon = menuButton.getElementsByTagName("a")[0];
+const menuCloseIcon = menuButton.getElementsByTagName("a")[1];
+const overlayMenu = document.getElementsByClassName("overlay-menu")[0];
 
 //? Toggles RoboFly animation and the play/pause icons
 function toggleRoboAnimation() {
@@ -27,9 +31,9 @@ function toggleRoboAnimation() {
 
 //? Listens to the 'end of scroll' event and triggers a fuction to check scroll position of the page and places the floating chat button accordingly
 window.onscrollend = () => {
-    let scrollPosition = htmlElement.scrollTop; //? Current (vertical) scroll position of the page
-    let windowHeight = window.innerHeight; //? Height of visible section of 
-    let scrollHeight = htmlElement.scrollHeight;//? Height of entire document page
+    let scrollPosition = document.scrollingElement.scrollTop; //? Current (vertical) scroll position of the page
+    let windowHeight = window.innerHeight; //? Height of visible section 
+    let scrollHeight = document.scrollingElement.scrollHeight;//? Height of entire document page
     let footerHeight = lowerFooterElement.clientHeight; //? Height of the Lower section of the page footer
 
     let unscrolledHeight = scrollHeight - (windowHeight + scrollPosition); //? Calculates the unscrolled height of the page
@@ -41,6 +45,20 @@ window.onscrollend = () => {
      * ? footerHeight - unscrolledHeight is basically the visible portion of the footer
      * ? '18px' is the gap between the floating button and the footer top
      *===================================================================================**/
-    if (unscrolledHeight < footerHeight) floatingChatButton.style.bottom = `${footerHeight - unscrolledHeight + 18}px`
-    else floatingChatButton.style.bottom = "36px";
+    floatingChatButton.style.bottom = unscrolledHeight < footerHeight ? `${footerHeight - unscrolledHeight + 18}px` : "36px";
+}
+
+function toggleOverLayMenu() {
+    if (overlayMenu.style.display != "block") {
+        overlayMenu.style.display = "block";
+        menuBarIcon.style.display = "none";
+        menuCloseIcon.style.display = "block";
+        document.scrollingElement.classList.add("hidden-scrollbar");
+    }
+    else {
+        overlayMenu.style.display = "none";
+        menuBarIcon.style.display = "block";
+        menuCloseIcon.style.display = "none";
+        document.scrollingElement.classList.remove("hidden-scrollbar");
+    }
 }
